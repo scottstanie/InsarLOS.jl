3d70eef3cbf014477a4aa3e4a3ccbdec575a57d0
scott <scott.stanie@gmail.com>
2019-10-08 14:46:10 -0500

    move grid funcs to Mapimages

--- src/decompose.jl
+++ src/decompose.jl
@@ -1,4 +1,5 @@
 using HDF5
+using MapImages
 
 function solve_east_up(asc_img, desc_img, asc_los_map, desc_los_map)
     # @show size(asc_los_map), size(desc_los_map), size(asc_img), size(desc_img)
@@ -51,17 +52,17 @@ function plot_eu(east, up; cmap="seismic_wide", vm=20, east_scale=1.0, title="")
     return fig, axes
 end
 
-function demo_east_up(fn="velocities_prune_l1.h5", dset="velos/1"; full=false, east_scale=1.0)
+function demo_east_up(fn="velocities_prune_l1.h5"; dset="velos/1", full=false, east_scale=1.0)
     if full
         asc_path, desc_path = ("/data1/scott/pecos/path78-bbox2/igrams_looked/", "/data4/scott/path85/stitched/igrams_looked/")
         asc_fname, desc_fname = map(x -> joinpath(x, fn), (asc_path, desc_path))
-        asc_img, desc_img = find_overlap_patches(asc_fname, desc_fname, "velos/1")
+        asc_img, desc_img = find_overlap_patches(asc_fname, desc_fname, dset)
 
-        asc_dem_rsc = Sario.load_dem_from_h5(asc_fname)
-        desc_dem_rsc = Sario.load_dem_from_h5(desc_fname)
+        asc_demrsc = Sario.load_dem_from_h5(asc_fname)
+        desc_demrsc = Sario.load_dem_from_h5(desc_fname)
         asc_los_map = permutedims(h5read(joinpath(asc_path, "los_map.h5"), "stack"), (2, 1, 3))
         desc_los_map = permutedims(h5read(joinpath(desc_path, "los_map.h5"), "stack"), (2, 1, 3))
-        asc_idxs, desc_idxs = find_overlap_idxs(asc_los_map, desc_los_map, asc_dem_rsc, desc_dem_rsc)
+        asc_idxs, desc_idxs = find_overlap_idxs(asc_los_map, desc_los_map, asc_demrsc, desc_demrsc)
 
         east, up = solve_east_up(asc_img, desc_img, asc_los_map[asc_idxs..., :], desc_los_map[desc_idxs..., :])
     else
@@ -73,21 +74,43 @@ function demo_east_up(fn="velocities_prune_l1.h5", dset="velos/1"; full=false, e
 end
 
 
-function find_overlap_idxs(asc_img, desc_img, asc_dem_rsc, desc_dem_rsc)
-    left, right, bottom, top = intersection_corners(asc_dem_rsc, desc_dem_rsc)
+function find_overlap_idxs(asc_img, desc_img, asc_demrsc, desc_demrsc)
+    left, right, bottom, top = intersection_corners(asc_demrsc, desc_demrsc)
     println(left, right, bottom, top)
 
     # asc_patch = asc_velo[32.3:30.71, -104.1:-102.31]
     # desc_patch = desc_velo[32.3:30.71, -104.1:-102.31]
 
-    row1, col1 = nearest_pixel(asc_dem_rsc, top, left)
-    row2, col2 = nearest_pixel(asc_dem_rsc, bottom, right)
+    row1, col1 = nearest_pixel(asc_demrsc, top, left)
+    row2, col2 = nearest_pixel(asc_demrsc, bottom, right)
     asc_idxs = (row1:row2, col1:col2)
 
     # asc_patch = asc_img[row1:row2, col1:col2]
 
-    row1, col1 = nearest_pixel(desc_dem_rsc, top, left)
-    row2, col2 = nearest_pixel(desc_dem_rsc, bottom, right)
+    row1, col1 = nearest_pixel(desc_demrsc, top, left)
+    row2, col2 = nearest_pixel(desc_demrsc, bottom, right)
+    desc_idxs = (row1:row2, col1:col2)
+    # desc_patch = desc_img[row1:row2, col1:col2]
+
+    return asc_idxs, desc_idxs
+    # return asc_patch, desc_patch
+end
+
+function find_overlaps(asc_img::MapImage, desc_img::MapImage)
+    left, right, bottom, top = intersection_corners(asc_demrsc, desc_demrsc)
+    println(left, right, bottom, top)
+
+    # asc_patch = asc_velo[32.3:30.71, -104.1:-102.31]
+    # desc_patch = desc_velo[32.3:30.71, -104.1:-102.31]
+
+    row1, col1 = nearest_pixel(asc_demrsc, top, left)
+    row2, col2 = nearest_pixel(asc_demrsc, bottom, right)
+    asc_idxs = (row1:row2, col1:col2)
+
+    # asc_patch = asc_img[row1:row2, col1:col2]
+
+    row1, col1 = nearest_pixel(desc_demrsc, top, left)
+    row2, col2 = nearest_pixel(desc_demrsc, bottom, right)
     desc_idxs = (row1:row2, col1:col2)
     # desc_patch = desc_img[row1:row2, col1:col2]
 
@@ -108,9 +131,9 @@ function find_overlap_patches(asc_fname::AbstractString, desc_fname::AbstractStr
     asc_img = permutedims(h5read(asc_fname, dset))
     desc_img = permutedims(h5read(desc_fname, dset))
 
-    asc_dem_rsc = Sario.load_dem_from_h5(asc_fname)
-    desc_dem_rsc = Sario.load_dem_from_h5(desc_fname)
-    asc_idxs, desc_idxs = find_overlap_idxs(asc_img, desc_img, asc_dem_rsc, desc_dem_rsc)
+    asc_demrsc = Sario.load_dem_from_h5(asc_fname)
+    desc_demrsc = Sario.load_dem_from_h5(desc_fname)
+    asc_idxs, desc_idxs = find_overlap_idxs(asc_img, desc_img, asc_demrsc, desc_demrsc)
 
     a, d = asc_img[asc_idxs...], desc_img[desc_idxs...]
     a, d = _mask_asc_desc(a, d)
@@ -135,18 +158,18 @@ end
 # end
 
 """Find the nearest row, col to a given lat and/or lon"""
-function nearest_pixel(dem_rsc, lat::AbstractFloat, lon::AbstractFloat)
-    @show lon - dem_rsc["x_first"]
-    @show dem_rsc["x_step"]
-    col_idx = 1 + (lon - dem_rsc["x_first"]) / dem_rsc["x_step"]
+function nearest_pixel(demrsc::DemRsc, lat::AbstractFloat, lon::AbstractFloat)
+    @show lon - demrsc.x_first
+    @show demrsc.x_step
+    col_idx = 1 + (lon - demrsc.x_first) / demrsc.x_step
     # out_row_col[2] = _check_bounds(col_idx_arr, ncols)
-    row_idx = 1 + (lat - dem_rsc["y_first"]) / dem_rsc["y_step"]
+    row_idx = 1 + (lat - demrsc.y_first) / demrsc.y_step
     # out_row_col[1] = _check_bounds(row_idx_arr, nrows)
 
     return Int.(round.((row_idx, col_idx)))
 end
-nearest_pixel(dem_rsc, lats::AbstractArray{AbstractFloat}, 
-              lons::AbstractArray{AbstractFloat}) = [nearest_pixel(dem_rsc, lat, lon)
+nearest_pixel(demrsc, lats::AbstractArray{AbstractFloat}, 
+              lons::AbstractArray{AbstractFloat}) = [nearest_pixel(demrsc, lat, lon)
                                                      for (lat, lon) in zip(lats, lons)]
 
 _max_min(a, b) = max(minimum(a), minimum(b))
@@ -162,8 +185,8 @@ function intersection_corners(dem1, dem2)
     """
     dem1 = _symdict(dem1)
     dem2 = _symdict(dem2)
-    corners1 = grid_corners(;dem1...)
-    corners2 = grid_corners(;dem2...)
+    corners1 = MapImages.grid_corners(;dem1...)
+    corners2 = MapImages.grid_corners(;dem2...)
     lons1, lats1 = zip(corners1...)
     lons2, lats2 = zip(corners2...)
     left = _max_min(lons1, lons2)
@@ -171,38 +194,5 @@ function intersection_corners(dem1, dem2)
     bottom = _max_min(lats1, lats2)
     top = _least_common(lats1, lats2)
     return left, right, bottom, top
-end
-
-
-function grid_corners(; kwargs...)
-    """Takes sizes and spacing from .rsc info, finds corner points in (x, y) form
-
-    Returns:
-        list[tuple[float]]: the corners of the latlon grid in order:
-        (top right, top left, bottom left, bottom right)
-    """
-    left, right, bot, top = grid_extent(;kwargs...)
-    return [(right, top), (left, top), (left, bot), (right, bot)]
-end
-
-
-"""
-Returns:
-    tuple[float]: the boundaries of the latlon grid in order:
-    (lon_left,lon_right,lat_bottom,lat_top)
-"""
-function grid_extent(;rows=nothing,
-                     cols=nothing,
-                     y_step=nothing,
-                     x_step=nothing,
-                     y_first=nothing,
-                     x_first=nothing,
-                     file_length=nothing,
-                     width=nothing,
-                     kwargs...)
-    rows = isnothing(rows) ? file_length : rows
-    cols = isnothing(cols) ? width : cols
-    @show cols
-    return (x_first, x_first .+ x_step * (cols - 1), y_first + y_step * (rows - 1), y_first)
 end
 
